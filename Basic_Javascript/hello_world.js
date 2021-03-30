918c71743bac136c90ac946b855c668f681a183b
Luis Garza <garzamedinagarza@gmail.com>
2021-03-29 22:57:07 -0500

    MVC, funcion de agregar todo completada

--- Basic_Javascript/hello_world.js
+++ Basic_Javascript/hello_world.js
@@ -1,1 +1,40 @@
 console.log('Hello World!');
+
+/* var vs let */
+
+// var puede ser declarado varias veces
+var age = 10;
+var age = 20;
+console.log(age);
+
+// let puede solo declarar una vez la variable
+let age_2 = 10;
+// let age_2 = 10; // si descomentamos daria error
+
+ //al declarar un objeto con var, let o const, este puede ser modificado
+const person = {
+    name: 'Juanito',
+    age : 20
+}
+
+console.log(person);
+
+person.age  = 21;
+person.country = 'México';
+
+console.log(person);
+
+
+/* funciones */
+
+function sum (a, b) {
+    console.log(a + b)
+}
+
+sum(10, 5);
+
+const sum_2 = (a, b) => {
+    console.log(a + b)
+}
+
+sum_2(10, 5);
---
+++ References.md
@@ -0,0 +1,24 @@
+
+
+# GIT
+
+## PUSH and PULL
+
+https://www.datacamp.com/community/tutorials/git-push-pull
+
+## Clone a repository
+
+git clone https://github.com/4tomik/JSTodoList.git
+
+
+# Node js
+
+Instalar servidor:
+```bash
+npm i -g serve
+```
+
+Ejecutamos servidor en puerto 3000
+```
+serve -l 3000
+```
