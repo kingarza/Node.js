20808a7be19e0db4f3c648b2aa2cacc2fcae61aa
Luis Garza <garzamedinagarza@gmail.com>
2021-04-05 18:33:31 -0500

    passing data into views, partials

--- Basics_NodeJS/Unit_2/app.js
+++ Basics_NodeJS/Unit_2/app.js
@@ -4,28 +4,32 @@ const express = require('express');
 const app = express();
 
 //register view engine
+//app.set('views', './views/');
 app.set('view engine', 'ejs');
-app.set('view engine', 'myviews');
+
 
 //listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // we specify the relative root with the 2nd parameter
-    res.sendFile('./views/index.html', { root : __dirname });
-})
+    const blogs = [
+      {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', { title: 'Home', blogs : blogs });
+});
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root : __dirname });
-})
+res.render('about', { title: 'About' });
+});
 
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-})
+app.get('/blogs/create', (req, res) => {
+res.render('create', { title: 'Create a new blog' });
+});
 
 //404 page
 // default case, This function is executed if none of the above functions worked
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root : __dirname })
+    res.status(404).render('404', { title: '404'});
 })
